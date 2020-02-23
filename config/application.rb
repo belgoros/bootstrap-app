1de82bc8b53ed19fe67044b697fd9f12b64a56bf
Serguei Cambour <s.cambour@gmail.com>
2020-02-23 13:37:12 +0100

    Generate Post scaffold

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,9 @@ module BootstrapApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    # Disable generation of controller specific asset files 
+    config.generators do |g|
+      g.assets false
+    end
   end
 end
